cd4f26d769ada5e366a035d7096623eb1b83aa3e
Konstantin Chukharev <lipen00@gmail.com>
2019-06-14 02:12:16 +0300

    Fix signatures, add flatMapError

--- src/main/kotlin/com/github/lipen/result/Result.kt
+++ src/main/kotlin/com/github/lipen/result/Result.kt
@@ -1,3 +1,5 @@
+@file:Suppress("unused")
+
 package com.github.lipen.result
 
 import kotlin.contracts.InvocationKind
@@ -28,25 +30,27 @@ sealed class Result<out V, out E : Exception> {
     }
 }
 
-inline fun <reified E : Exception, R> runCatching(block: () -> R): Result<R, E> {
+inline fun <R, reified E : Exception> runCatching(block: () -> R): Result<R, E> {
     contract {
-        callsInPlace(block, InvocationKind.EXACTLY_ONCE)
+        callsInPlace(block, InvocationKind.AT_MOST_ONCE)
     }
     return try {
         Result.success(block())
     } catch (ex: Exception) {
-        (ex as? E)?.let { Result.failure(it) } ?: throw ex
+        (ex as? E)?.let { Result.failure(it) }
+            ?: throw IllegalStateException("runCatching caught an unexpected exception", ex)
     }
 }
 
-inline fun <T, reified E : Exception, R> T.runCatching(block: T.() -> R): Result<R, E> {
+inline fun <T, R, reified E : Exception> T.runCatching(block: T.() -> R): Result<R, E> {
     contract {
-        callsInPlace(block, InvocationKind.EXACTLY_ONCE)
+        callsInPlace(block, InvocationKind.AT_MOST_ONCE)
     }
     return try {
         Result.success(block())
     } catch (ex: Exception) {
-        (ex as? E)?.let { Result.failure(it) } ?: throw ex
+        (ex as? E)?.let { Result.failure(it) }
+            ?: throw IllegalStateException("runCatching caught an unexpected exception", ex)
     }
 }
 
@@ -77,7 +81,7 @@ inline fun <V, E : Exception> Result<V, E>.onFailure(block: (E) -> Unit): Result
     return this
 }
 
-inline fun <V : R, E : Exception, R> Result<V, E>.getOrElse(onFailure: (Exception) -> R): R {
+inline fun <V : R, E : Exception, R> Result<V, E>.getOrElse(onFailure: (E) -> R): R {
     contract {
         callsInPlace(onFailure, InvocationKind.AT_MOST_ONCE)
     }
@@ -86,26 +90,6 @@ inline fun <V : R, E : Exception, R> Result<V, E>.getOrElse(onFailure: (Exceptio
 
 fun <V : R, E : Exception, R> Result<V, E>.getOrDefault(default: R): R =
     fold({ it }, { default })
-
-inline fun <V : R, reified E : Exception, R> Result<V, E>.map(transform: (V) -> R): Result<R, E> {
-    contract {
-        callsInPlace(transform, InvocationKind.AT_MOST_ONCE)
-    }
-    return when (this) {
-        is Result.Success -> Result.success(transform(value))
-        is Result.Failure -> this
-    }
-}
-
-inline fun <V : R, reified E : Exception, R> Result<V, E>.mapCatching(transform: (V) -> R): Result<R, E> {
-    contract {
-        callsInPlace(transform, InvocationKind.AT_MOST_ONCE)
-    }
-    return when (this) {
-        is Result.Success -> runCatching { transform(value) }
-        is Result.Failure -> this
-    }
-}
 
 inline fun <V, E : Exception, R> Result<V, E>.flatMap(transform: (V) -> Result<R, E>): Result<R, E> {
     contract {
@@ -117,17 +101,41 @@ inline fun <V, E : Exception, R> Result<V, E>.flatMap(transform: (V) -> Result<R
     }
 }
 
-inline fun <V, E : Exception, F : Exception> Result<V, E>.mapError(transform: (E) -> F): Result<V, F> {
+inline fun <V, E : Exception, R> Result<V, E>.map(transform: (V) -> R): Result<R, E> {
+    contract {
+        callsInPlace(transform, InvocationKind.AT_MOST_ONCE)
+    }
+    return flatMap { value -> Result.success(transform(value)) }
+}
+
+inline fun <V, E : F, R, reified F : Exception> Result<V, E>.mapCatching(transform: (V) -> R): Result<R, F> {
+    contract {
+        callsInPlace(transform, InvocationKind.AT_MOST_ONCE)
+    }
+    return when (this) {
+        is Result.Success -> runCatching { transform(value) }
+        is Result.Failure -> this
+    }
+}
+
+inline fun <V, E : Exception, F : Exception> Result<V, E>.flatMapError(transform: (E) -> Result<V, F>): Result<V, F> {
     contract {
         callsInPlace(transform, InvocationKind.AT_MOST_ONCE)
     }
     return when (this) {
         is Result.Success -> this
-        is Result.Failure -> Result.Failure(transform(error))
+        is Result.Failure -> transform(error)
     }
 }
 
-inline fun <V : R, E : Exception, R> Result<V, E>.recover(transform: (E) -> R): Result<R, E> {
+inline fun <V, E : F, F : Exception> Result<V, E>.mapError(transform: (E) -> F): Result<V, F> {
+    contract {
+        callsInPlace(transform, InvocationKind.AT_MOST_ONCE)
+    }
+    return flatMapError { error -> Result.failure(transform(error)) }
+}
+
+inline fun <V : R, E : Exception, R> Result<V, E>.recover(transform: (E) -> R): Result<R, Nothing> {
     contract {
         callsInPlace(transform, InvocationKind.AT_MOST_ONCE)
     }
@@ -137,7 +145,7 @@ inline fun <V : R, E : Exception, R> Result<V, E>.recover(transform: (E) -> R): 
     }
 }
 
-inline fun <V : R, reified E : Exception, R> Result<V, E>.recoverCatching(transform: (E) -> R): Result<R, E> {
+inline fun <V : R, E : Exception, R, reified F : Exception> Result<V, E>.recoverCatching(transform: (E) -> R): Result<R, F> {
     contract {
         callsInPlace(transform, InvocationKind.AT_MOST_ONCE)
     }
